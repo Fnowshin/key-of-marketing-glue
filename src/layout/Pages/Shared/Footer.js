78d273a36f01dd90314ce2bd6201c8cbd71c3d4b
Fnowshin <fnowshin5@gmail.com>
2022-10-25 18:58:17 +0600

    outline-created

--- src/layout/Pages/Shared/Footer.js
+++ src/layout/Pages/Shared/Footer.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+
 
 const Footer = (props) => {
     return (
-        <div>
-            <p> @copyright reserved </p>
-        </div>
+        <Card className="text-center">
+        
+        <Card.Body>
+          <Card.Title>Special title treatment</Card.Title>
+          <Card.Text>
+            With supporting text below as a natural lead-in to additional content.
+          </Card.Text>
+          <Button variant="danger">Go somewhere</Button>
+        </Card.Body>
+        
+      </Card>
     );
 };
 
