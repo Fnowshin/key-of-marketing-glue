53ce298e066d6e4130740b6af44df6e6fd0951ab
Fnowshin <fnowshin5@gmail.com>
2022-10-25 20:10:49 +0600

    home-page

--- src/layout/Pages/Home/Components/Slider.js
+++ src/layout/Pages/Home/Components/Slider.js
@@ -1,9 +1,35 @@
 import React from 'react';
+import Carousel from 'react-bootstrap/Carousel'
+import Picture1 from '../images/slider-pic-1.png'
+import Picture2 from '../images/slider-pic-2.png'
+import Picture3 from '../images/slider-pic-3.jpg'
 
 const Slider = (props) => {
     return (
         <div>
-            <h4> Sliding images will be shown here </h4>
+            <Carousel className='my-5'>
+                <Carousel.Item>
+                    <img
+                    className="d-block w-100"
+                    src={Picture1}
+                    alt="First slide"
+                    />
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                    className="d-block w-100"
+                    src={Picture2}
+                    alt="Second slide"
+                    />
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                    className="d-block w-100"
+                    src={Picture3}
+                    alt="Third slide"
+                    />
+                </Carousel.Item>
+    </Carousel>
         </div>
     );
 };
